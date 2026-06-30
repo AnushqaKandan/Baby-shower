aad5ae3ebdd75a99226249682d7c33bbef8e78af
AnushqaKandan <anushqakandan03@gmail.com>
2026-06-30 17:04:20 +0200

    added js for the rsvp on mobile

--- script/script.js
+++ script/script.js
@@ -1,4 +1,27 @@
-localStorage.setItem("rsvpSubmitted", "true");
-if (localStorage.getItem("rsvpSubmitted")) {
-    // Disable the form
-}
+// localStorage.setItem("rsvpSubmitted", "true");
+// if (localStorage.getItem("rsvpSubmitted")) {
+//     // Disable the form
+// }
+
+const button = document.querySelector(".floating-rsvp");
+const rsvpSection = document.querySelector(".rsvp-section");
+
+window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY;
+
+    // show after scrolling 200px
+    if (scrollY > 200) {
+        button.classList.add("show");
+    } else {
+        button.classList.remove("show");
+    }
+
+    // hide when RSVP section is in view
+    const rsvpTop = rsvpSection.getBoundingClientRect().top;
+
+    if (rsvpTop < window.innerHeight / 2) {
+        button.classList.add("hide");
+    } else {
+        button.classList.remove("hide");
+    }
+});
